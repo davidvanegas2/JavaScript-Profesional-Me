43aa4e5da43def059f326c35ea08dd874ece8a8e
David Vanegas <david-vanegas@javeriana.edu.co>
2020-09-27 09:40:58 -0500

    Plugin AutoPlay funcionando

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 import mediaPlayer from "./mediaPlayer.js"
+import AutoPlay from '../plugins/AutoPlay.js'
 
 const video = document.querySelector(
     "video" /* Defini en el script el tag de video del HTML */
@@ -6,7 +7,11 @@ const video = document.querySelector(
 const button = document.querySelector(
     "button" /* Defini en el script el tag de button del HTML */
 );
+const muteButton = document.querySelector(
+    ".muteButton"
+);
 //Defini el player para poder usarlo de manera global en el script
-const player = new mediaPlayer({ el: video });
+const player = new mediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 button.onclick = () => player.play();
+muteButton.onclick = () => player.isMuted();
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,10 +1,28 @@
 function mediaPlayer(config) {
     this.media = config.el;
-}
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+};
+
+mediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
 
 mediaPlayer.prototype.play = function() {
-    if (this.media.paused) this.media.play();
-    else this.media.pause();
+    if (this.media.paused)
+        this.media.play();
+    else
+        this.media.pause();
+};
+
+mediaPlayer.prototype.isMuted = function() {
+    if (this.media.muted)
+        this.media.muted = false;
+    else
+        this.media.muted = true;
 };
 
 export default mediaPlayer;
